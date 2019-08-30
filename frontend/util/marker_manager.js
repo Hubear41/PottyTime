7af17b7f7b9fc2ddc326887f1a557347430b12ff
Dennis Hu <ddh241@gmail.com>
2019-08-30 15:28:12 -0400

    edit updateMarkers method for MarkerManager

--- frontend/util/marker_manager.js
+++ frontend/util/marker_manager.js
@@ -4,8 +4,24 @@ class MarkerManager {
         this.markers = {};
     }
 
-    updateMarkers(benches) {
-        console.log("time to update")
+    updateMarkers(bathrooms) {
+        bathrooms.forEach( bathroom => {
+            if ( !this.markers[bathroom.id] ) {
+                this.createMarkerFromBathroom(bathroom);
+            }
+        });
+    }
+
+    createMarkerFromBathroom(bathroom) {
+        const { lat, lng, name, id } = bathroom;
+
+        const marker = new.google.maps.Marker({
+            position: { lat, lng }, 
+            title: name,
+        });
+
+        marker.setMap(this.map);
+        this.markers[id] = marker;
     }
 }
 
