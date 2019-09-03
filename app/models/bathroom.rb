d6af2f9e4b049f4ad3cc71e01fe569aa96efe078
Dennis Hu <ddh241@gmail.com>
2019-09-03 12:24:48 -0400

    fix in_bounds query

--- app/models/bathroom.rb
+++ app/models/bathroom.rb
@@ -2,17 +2,14 @@ class Bathroom < ApplicationRecord
     validates :name, :lat, :lng, presence: true
 
     def self.in_bounds(bounds)
-        
-        northEast = bounds[:northEast]
-        southWest = bounds[:southWest]
-
-        Bathroom.where("lat > ? AND
-                        lat < ? AND
-                        lng > ? AND
-                        lng < ?",
-                        southWest["lat"],
-                        northEast["lat"],
-                        southWest["lng"],
-                        northEast["lng"])
+        self.where("lat > ? AND
+                    lat < ? AND
+                    lng > ? AND
+                    lng < ?",
+                    bounds[:southWest][:lat],
+                    bounds[:northEast][:lat],
+                    bounds[:southWest][:lng],
+                    bounds[:northEast][:lng]
+        )
     end
 end
