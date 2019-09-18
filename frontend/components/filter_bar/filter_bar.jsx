d04ab19723199b569657b427ceb79dcbd41cc10f
Dennis Hu <ddh241@gmail.com>
2019-09-18 17:08:09 -0400

    add "All" button to filters

--- frontend/components/filter_bar/filter_bar.jsx
+++ frontend/components/filter_bar/filter_bar.jsx
@@ -3,10 +3,15 @@ import FilterBarItem from './filter_bar_item';
 
 const FilterBar = props => {
     const { categories, updateFilter, filteredCategoryIds, hidden } = props;
+    const hasNoFilters = Boolean(filteredCategoryIds.length <= 0);
 
     useEffect(() => {
         props.requestCategories();
     }, []);
+
+    const handleChangeAll = e => {
+        props.updateFilter("categoryIds", []);
+    }
 
     const categoryItems = categories.map( (category, idx) => {
         return (
@@ -27,13 +32,33 @@ const FilterBar = props => {
         <div 
           id="filter-bar-wrapper" 
           style={filterStyle}
+        class={hidden ? "filters-disappear" : "filters-appear"}
         >   
-            <button id="filter-clear-btn">
-                Clear All
-            </button>
+            <aside id="filter-all-btn">
+                <input
+                    type="checkbox"
+                    id={"category-all"}
+                    className="filter-cbox"
+                    onChange={handleChangeAll}
+                    checked={hasNoFilters}
+                    value="All"
+                />
+                <label
+                    className="cbox-label"
+                    htmlFor={"category-all"}
+                >
+                    <div className="cbox-text">
+                        <div className="svg-container">
+                            <svg viewBox="0 0 44 44">
+                                <path d="M14,24 L21,31 L39.7428882,11.5937758 C35.2809627,6.53125861 30.0333333,4 24,4 C12.95,4 4,12.95 4,24 C4,35.05 12.95,44 24,44 C35.05,44 44,35.05 44,24 C44,19.3 42.5809627,15.1645919 39.7428882,11.5937758" transform="translate(-2.000000, -2.000000)"></path>
+                            </svg>
+                        </div>
+                        <p>All</p>
+                    </div>
+                </label>
+            </aside>
             <ul 
               id="category-filter-list"
-              class={hidden ? "filters-disappear" : "filters-appear"}
             >
                 {categoryItems}
             </ul>
