be7ba15324fd2ea5a1fa457ef78011f4aa77ff38
Dennis Hu <ddh241@gmail.com>
2019-11-13 16:42:20 -0500

    update fonts for footer

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -4,9 +4,18 @@ const Footer = () => (
   <footer id="footer">
     <span className="footer-text">
       Potty Time app created by Dennis Hu and Elvis Yuan
+      <i className="footer-subtitle">
+        {" 2019"}
+        <i className="far fa-copyright"></i>
+      </i>
     </span>
+
     <aside className="footer-social-links">
-      <a className="footer-link-item" href="#">
+      <a
+        className="footer-link-item"
+        href="https://github.com/Hubear41/PottyTime"
+        target="_blank"
+      >
         <i className="fab fa-github"></i>
       </a>
     </aside>
